45fc2e35d8bf82d46bf5ba823ba4e0bed91e719e
Tan Nguyen Hoang <hoangtan22th@gmail.com>
2024-04-14 19:17:28 +0700

    aa

--- js/login.js
+++ js/login.js
@@ -9,7 +9,7 @@ window.onload = function () {
         var user = localStorage.getItem(username);
         var data = JSON.parse(user);
         if(user == null){
-            alert("Tài khoản hoặc mật khẩu không chính xác!!")
+            alert("ĐM nhập sai!!")
         }
         else if(username == data.username && password == data.password)
         {
@@ -19,7 +19,7 @@ window.onload = function () {
             window.location.href = "../html/home.html";
         }
         else{
-            alert("Vui lòng nhập tài khoản và mật khẩu");
+            alert("Đm nhập không đúng");
         }
     })
 
